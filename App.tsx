1806a3616956422fbaee4ccc824b7d902967bee6
Darlan Costa <darlan.costa@mazzafc.tech>
2023-05-01 18:53:51 -0300

    add tailwind

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Arquivo com DB</Text>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue1: '#404258',
+        gray1: '#333333',
+        gray2: '#a6a6a6',
+        orange: '#f09a36',
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    },
+  },
+  plugins: [],
+}
