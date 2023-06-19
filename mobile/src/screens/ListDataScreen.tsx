9d6c7561f9f20d0f81a1070a926e4c16fcdce9b9
F3rn4nd0o <fernandovasconcelosmelo@outlook.com>
2023-06-19 18:57:10 -0300

    Add useFocusEffct ListData

--- mobile/src/screens/ListDataScreen.tsx
+++ mobile/src/screens/ListDataScreen.tsx
@@ -1,27 +1,24 @@
 import {  DataCardProps } from '@components/DataCard';
 import { VStack } from 'native-base';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import storage from '../database/storage';
 import { Button } from '@components/Button';
 import { DataList } from '@components/DataList';
 import { useData } from '@hooks/useData';
+import { useFocusEffect } from '@react-navigation/native';
 
 export const ListDataScreen = () => {
 
-  const { handleDeleteCollect, handleListCollect, data } = useData();
+  const { handleListCollect, data } = useData();
 
-  function handlePress() {
-    handleListCollect()
-  }
-
-  function handleDelete() {
-    handleDeleteCollect()
-  }
+  useFocusEffect(
+    useCallback(() => {
+      handleListCollect()
+    },[])
+  )
   
   return (
     <VStack flex={1} backgroundColor='bgColor' justifyContent='center' alignItems='center'>
-      <Button title="List" w={32} onPress={handlePress} />
-      <Button title="Delete" w={32} onPress={handleDelete} />
       <DataList data={data} />
     </VStack>
   );
