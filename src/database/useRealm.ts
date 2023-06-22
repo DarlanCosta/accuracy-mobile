b42c3611a11a77006cacf10ba8a95f2815a6231a
F3rn4nd0o <99568451+F3rn4nd0o@users.noreply.github.com>
2023-06-22 16:30:14 -0300

    Delete useRealm.ts

--- src/database/useRealm.ts
+++
@@ -1,9 +0,0 @@
-import Realm from "realm";
-import { DataSchema } from "./schemas/DataSchema";
-import { UserSchema } from "./schemas/UserSchema";
-import { AuthTokenSchema } from "./schemas/AuthTokenSchema";
-
-export const getRealm = async () => await Realm.open({
-  path: "accuracy-app",
-  schema: [DataSchema, UserSchema, AuthTokenSchema ]
-})
