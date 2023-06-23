742c54fc5f1c68813549bc41fb54039f55d47ae8
F3rn4nd0o <99568451+F3rn4nd0o@users.noreply.github.com>
2023-06-23 01:25:22 -0300

    Config RealmDB

--- App.tsx
+++ App.tsx
@@ -9,28 +9,33 @@ import { AuthContextProvider } from '@contexts/AuthContext';
 
 import { NativeBaseProvider } from 'native-base'
 
+import { AppProvider } from '@realm/react';
+
 import { Routes } from "./src/routes";
 import { THEME } from "./src/theme";
 
 import { DataContextProvider } from "@contexts/DataContext";
-import { RealmProvider } from "@database/index";
+import { RealmProvider, syncConfig } from "./src/lib/realm/index";
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
   return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <AuthContextProvider>
-        <DataContextProvider>
-          <RealmProvider>
-            <Routes />
-          </RealmProvider>
-        </DataContextProvider>
-      </AuthContextProvider>
-    </NativeBaseProvider>
+    <AppProvider id={'application-0-ozhjl'}>
+      <NativeBaseProvider theme={THEME}>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <AuthContextProvider>
+          <DataContextProvider>
+            <RealmProvider sync={syncConfig} fallback={Loading}>
+              <Routes />
+            </RealmProvider>
+          </DataContextProvider>
+        </AuthContextProvider>
+      </NativeBaseProvider>
+    </AppProvider>
   );
 }
